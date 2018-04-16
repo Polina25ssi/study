6ce59c0a36fcef0c302d6fcd311475222b332388
Polina25ssi <36626710+Polina25ssi@users.noreply.github.com>
2018-04-16 14:32:59 +0300

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,17 @@
+from math import *
+import math
+st = input("Enter the formule: ")
+for i in range(len(st)):
+    if (st[i]=='+'):
+        i=i+1
+        r=S2()
+    elif (st[i]=='-'):
+        i = i + 1
+        r = -S2()
+    else:
+        r=S2()
+for
+    if
+
+
+print(eval(input("Enter the formule: ")))
